ce97e498231bdf1867c7380dd4ae951adb4deb7a
Victor Parizio <victorparizio@hotmail.com>
2023-01-12 16:10:35 -0300

    feat (App.js, components): Criando props
    Foi criado props para o novo componente SayMyName e Pessoa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Victor'
-  const newName = name.toUpperCase()
 
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
+  const nome = 'Maria'
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {name}</p>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt='Minha imagem'/>
-      <HelloWorld />
+      <SayMyName nome='Victor' />
+      <SayMyName nome='João' />
+      <SayMyName nome={nome} />
+      <Pessoa
+        nome='Rodrigo'
+        idade='28'
+        profissao='Programador'
+        foto='https://via.placeholder.com/150' />
     </div>
   );
 }
