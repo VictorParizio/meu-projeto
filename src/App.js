332cf2dae7b889c684824f8f35c4bd6292605be6
Victor Parizio <victorparizio@hotmail.com>
2023-01-13 19:21:18 -0300

    feat (OutraLista.js): Renderização de listas
    Foi adicionado um componente de lista com uma condição ternária para aprensentar os dados, quando a lista tiver um item ou mais e/ou uma mensagem para o caso de lista vazia.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
+
+  const meusItens = ['React', 'Veu', 'Angular']
+
   return (
     <div className="App">
-      <h1>Renderização Condicional</h1>
-      <Condicional />
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens} />
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={[]} />
     </div>
   );
 }
