6087b567d1bb8a795260243bbf115aee5859089d
Victor Parizio <victorparizio@hotmail.com>
2023-01-14 09:40:02 -0300

    feat (Footer): React Icons
    Foram adicionados e estilizados icones no rodapé

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,22 @@
-function Footer () {
-    return(
-        <footer>Rodapé</footer>
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <ul className='social_list'>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+            </ul>
+            <p>Nosso rodapé</p>
+        </footer>
     )
 }
 
